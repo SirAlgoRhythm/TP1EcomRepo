2b2a37c788678c035d18d2c6e50f5c25be93f323
Antoine <AntoineRioux.AR@gmail.com>
2023-02-23 11:42:07 -0500

    le visuel de la page d'interf gén est terminé

---
+++ TheWebsite/wwwroot/js/my.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    // Récupération de l'élément range et de l'élément textbox
+    var range = $('#range');
+    var textbox = $('#textbox');
+
+    // Mise à jour de la valeur du textbox lorsqu'on bouge le range
+    range.on('input', function () {
+        var value = range.val();
+        textbox.val(value);
+    });
+});
