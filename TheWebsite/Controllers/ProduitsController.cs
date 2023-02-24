48c8b1a90546f9e87d20839d414f2e5f16b7e1e8
Antoine <AntoineRioux.AR@gmail.com>
2023-02-23 20:32:11 -0500

    l'interface visuelle pour la gestion de produits est terminée

---
+++ TheWebsite/Controllers/ProduitsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheWebsite.Controllers
+{
+    public class ProduitsController : Controller
+    {
+        public IActionResult GestionProduits()
+        {
+            return View();
+        }
+    }
+}
