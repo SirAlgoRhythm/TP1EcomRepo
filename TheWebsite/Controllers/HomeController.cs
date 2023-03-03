81c89151f428915725b3cde7e941c46fff9a107d
Elizabeth0014 <115196592+Elizabeth0014@users.noreply.github.com>
2023-03-02 21:57:08 -0500

    TopBanner fonctionne, afficher les produits dans home/index

--- TheWebsite/Controllers/HomeController.cs
+++ TheWebsite/Controllers/HomeController.cs
@@ -5,9 +5,16 @@ namespace TheWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        public TheWebsiteContext DbContext { get; set; }
+        public HomeController()
+        {
+            this.DbContext = new TheWebsiteContext();
+        }
         public IActionResult Index(Models.Utilisateur utilisateur)
         {
             IndexVM indexVM = new IndexVM(utilisateur);
+            //Chercher tous les produits des vendeurs
+            indexVM.ProduitList = this.DbContext.Produits.ToList();
             return View(indexVM);
         }
     }
