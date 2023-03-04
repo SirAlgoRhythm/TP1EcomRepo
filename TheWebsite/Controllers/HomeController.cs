fccbb6c9ad9df0799b9bbb772a4745bdcafd784d
Antoine <AntoineRioux.AR@gmail.com>
2023-03-04 15:24:38 -0500

    Ajout de la recherche de produit

--- TheWebsite/Controllers/HomeController.cs
+++ TheWebsite/Controllers/HomeController.cs
@@ -12,10 +12,27 @@ namespace TheWebsite.Controllers
         }
         public IActionResult Index(Models.Utilisateur utilisateur)
         {
-            IndexVM indexVM = new IndexVM(utilisateur);
+            IndexVM indexVM = new IndexVM(utilisateur, null);
             //Chercher tous les produits des vendeurs
             indexVM.ProduitList = this.DbContext.Produits.ToList();
             return View(indexVM);
         }
+
+        [HttpGet]
+        public IActionResult Recherche(string search, Guid utilisateurId)
+        {
+            List<Produit> produitList = new List<Produit>();
+            produitList = this.DbContext.Produits.Where(p => p.Title.Contains(search) || p.Description.Contains(search)).ToList();
+
+            Utilisateur utilisateur = this.DbContext.Utilisateurs.Find(utilisateurId);
+            IndexVM indexVM = new IndexVM(utilisateur, produitList);
+
+            return View("index", indexVM);
+        }
+
+        //public IActionResult AjoutPanier(Models.Produit produit, Utilisateur utilisateur)
+        //{
+
+        //}
     }
 }
--- TheWebsite/Models/IndexVM.cs
+++ TheWebsite/Models/IndexVM.cs
@@ -5,9 +5,10 @@
         public Utilisateur UtilisateurActif { get; set; }
         public List<Produit> ProduitList { get; set; }
 
-        public IndexVM(Utilisateur u)
+        public IndexVM(Utilisateur u, List<Produit> produits)
         {
             this.UtilisateurActif = u;
+            this.ProduitList = produits;
         }
 
     }
